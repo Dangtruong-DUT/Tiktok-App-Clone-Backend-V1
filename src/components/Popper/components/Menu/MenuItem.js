98e4f756940c3847e8891d21b11510d265de679f
TapLamIT <ngdtrg281@gmail.com>
2025-01-10 02:30:03 +0700

    xây dựng menu da lớp

--- src/components/Popper/components/Menu/MenuItem.js
+++ src/components/Popper/components/Menu/MenuItem.js
@@ -4,12 +4,16 @@ import Button from "@/components/button";
 
 const cx = classNames.bind(Styles);
 
-function MenuItem({data}) {
-    return ( 
-        <Button className= {cx('menu-item')} leftIcon={data.icon} to={data.to}>
+function MenuItem({ data, onClick }) {
+    return (
+        <Button className={cx('list-items__item')}
+            leftIcon={data.icon}
+            to={data.to}
+            onClick={onClick}
+        >
             {data.title}
         </Button>
-     );
+    );
 }
 
 export default MenuItem;
