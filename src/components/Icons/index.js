f9a5ca35c9c946394f57dc248fff83e2a2ec675e
TapLamIT <ngdtrg281@gmail.com>
2025-01-10 18:55:56 +0700

    embedded font ticktok, modularization Icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,18 @@
+export { default as LogoBrand } from './LogoBrand';
+export { default as PlusIcon } from './PlusIcon';
+export { default as ClearIcon } from './ClearIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as SpinnerIcon } from './SpinnerIcon';
+export { default as ThreeDotIcon } from './ThreeDotIcon';
+export { default as PersonIcon } from './PersonIcon';
+export { default as CoinIcon } from './CoinIcon';
+export { default as CreateIcon } from './CreateIcon';
+export { default as LightBulbIcon } from './LightBulbIcon';
+export { default as BusinessSuiteIcon } from './BusinessSuiteIcon';
+export { default as SettingIcon } from './SettingIcon';
+export { default as InfoIcon } from './InfoIcon';
+export { default as WaningMoonIcon } from './WaningMoonIcon';
+export { default as LogoutIcon } from './LogoutIcon';
+export { default as LetterAIcon } from './LetterAIcon';
+
+
